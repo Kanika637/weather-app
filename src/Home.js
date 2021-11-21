8c010bc49d922fa5afd245c778e435615742b388
kanika <kanika@example.com>
2021-11-21 16:07:05 +0530

    New changes in home

--- src/Home.js
+++ src/Home.js
@@ -3,12 +3,18 @@ import "./home.css"
 function Home() {
     return (
         <div className="home">
+            <div className="heading"><h1>WEATHER APP</h1></div>
+            <div className="sub_line">
+                <h4>Check the weather</h4>
+            </div>
             <div className="home_container">
                 <form>
-                    <input type="text" placeholder="Enter your country name"></input>
+
+                    <input type="text" placeholder="Enter your country"></input>
                     <button className="button">Search</button>
                 </form>
             </div>
+            
             
         </div>
     )
